322e76d1f9469f40ac092372683f038620100672
Antonio Rosales <jose.rosales.mald@gmail.com>
2024-02-11 14:08:22 -0600

    Vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const firebaseKeys = [
+  "SOME_KEY_IN_YOUR_ENV_FILE",
+  "SOME_OTHER_KEY_IN_YOUR_ENV_FILE",
+];
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv = {};
+  firebaseKeys.forEach(key => processEnv[key] = env[key]);
+
+  return {
+    define: {
+      'process.env': processEnv
+    },
+    plugins: [react()],
+  }
 })
